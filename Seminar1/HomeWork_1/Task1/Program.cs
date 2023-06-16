c69e9e2990d3eb14b046bd9b9b8dde89b2bbb857
PtycaOo <DenKuzia5155@gmail.com>
2023-06-16 20:44:04 +0300

    новый репозиторий

---
+++ Seminar1/HomeWork_1/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3*/
+
+Console.WriteLine("Введите первый оператор");
+int numberInt_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второй оператор");
+int numberInt_2 = Convert.ToInt32(Console.ReadLine());
+if(numberInt_1 > numberInt_2){
+    Console.WriteLine("Наибольшее число: "+ numberInt_1);
+} else {
+    Console.WriteLine("Наибольшее число: "+ numberInt_2);
+}
+
+
---
+++ Seminar1/HomeWork_1/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+Console.WriteLine("Введите число 1");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 3");
+int n3 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if(n1 > n2){
+    max = n1;
+} else {
+    max = n2;
+}
+if(n3 > max){
+    max = n3;
+}
+Console.WriteLine("Наибольшее из 3х чисел: " + max);
+
