350747847f280a3d3346c1b144c1c8ca217837e5
PtycaOo <DenKuzia5155@gmail.com>
2023-06-16 22:32:00 +0300

    А не полиндром ли это?

---
+++ seminar3/HomeWork_3/Task_1.3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = x;
+int reverce = 0;
+while(x !=0){
+    reverce = reverce*10 + x%10;
+    x/=10;
+}
+
+if(y == reverce){
+    Console.WriteLine("Полиндром");
+} else {
+    Console.WriteLine("Найн, нихт");
+}
