5a1adedff2844c9484b4c564b24a0ec4ed05a9db
PtycaOo <DenKuzia5155@gmail.com>
2023-06-16 22:45:21 +0300

    что то поменялось

---
+++ seminar3/Task_19/Program.cs
@@ -0,0 +1,19 @@
+﻿int num = 0;
+string str = "";
+Console.WriteLine("Введите число:");
+if (Int32.TryParse(Console.ReadLine(), out num))
+{
+
+    if (num == 1) { str = "X - 0 до бесконечность, Y - 0 до бесконечность"; 
+    } else if (num == 2) {
+        str = "X - 0 - минус бесконечность, Y - 0 - бесконечность"; 
+    } else if (num == 3) { 
+        str = "X - 0 - минус бесконечность, Y - 0 - минус бесконечность"; 
+    } else if (num == 4) { 
+        str = "X - 0 - бесконечность, Y - 0 - минус бесконечность"; 
+    }
+    Console.WriteLine(str);
+}
+else {
+    Console.WriteLine("Не число!");
+}
