fd4c20d9e6876da62a718e3086114fd815a4b90b
PtycaOo <DenKuzia5155@gmail.com>
2023-06-16 21:49:09 +0300

    Опять те же задачи

---
+++ seminar3/Task_21/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число 1");
+double num_1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+double num_2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(num_1);
