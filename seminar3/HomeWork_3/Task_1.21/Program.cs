24151250349d360f572d7d7fb657f5b1b7ca6638
PtycaOo <DenKuzia5155@gmail.com>
2023-06-17 11:21:56 +0300

    решил задачу с поиском 3д расстояния

---
+++ seminar3/HomeWork_3/Task_1.21/Program.cs
@@ -0,0 +1,12 @@
+﻿int [] arr_1 = new int [3];
+int [] arr_2 = new int [3];
+Console.WriteLine("Введите 3 коодританы x,y,z первой точки"); 
+for(int i = 0; i < 3; i++){
+    arr_1[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Введите 3 коодританы x,y,z второй точки");
+for(int j = 0; j < 3; j++){
+    arr_2[j] = Convert.ToInt32(Console.ReadLine());
+}
+double res = Math.Round(Math.Sqrt(Math.Pow(arr_2[0]-arr_1[0],2) + Math.Pow(arr_2[1]-arr_1[1],2) + Math.Pow(arr_2[2]-arr_1[2],2)),2);
+Console.WriteLine(res);
