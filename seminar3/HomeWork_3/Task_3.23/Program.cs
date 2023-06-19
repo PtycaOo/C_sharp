dbae59abd0c0ebee1ef369ee2e8260f8eafdc51e
PtycaOo <DenKuzia5155@gmail.com>
2023-06-19 19:53:23 +0300

    Внесите число!!!

---
+++ seminar3/HomeWork_3/Task_3.23/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Внесите число!!!");
+int N = Convert.ToInt32(Console.ReadLine());
+for(int i = 1; i <= N; i++){
+    double res = Math.Pow(i,3);
+    Console.Write(res);
+    if(i < N){
+        Console.Write(", ");
+    }
+    
+}
