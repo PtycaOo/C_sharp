9f29e4b03d48f5ad4f8fa2d4653fb06a98deff12
PtycaOo <DenKuzia5155@gmail.com>
2023-06-20 21:00:47 +0300

    Первая задача с функциями

---
+++ Seminar4/task_24/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36*/
+
+int ReadInt(){
+    Console.WriteLine("Введите целое число");
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
+}
+
+int sum(int a){
+    int sum = 0;
+    for(int i = 0; i <= a; i++){
+        sum += i;
+    }
+    return sum;
+}
+int value_1 = ReadInt();
+int res = sum(value_1);
+Console.WriteLine($"Сумма чисел от 1 до {value_1} равно {res}");
