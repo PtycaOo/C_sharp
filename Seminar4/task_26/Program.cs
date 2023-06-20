d86677f6c6350331df0d05d0c1f433e22285a8c0
PtycaOo <DenKuzia5155@gmail.com>
2023-06-20 21:56:29 +0300

    закончил семинар 4

---
+++ Seminar4/task_26/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5*/
+
+int ReadInt(){
+    Console.WriteLine("Введите целое число");
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
+}
+
+int number_count(int x){
+int count = 0;
+    while(x !=0){
+        x/=10;
+        count += 1;
+    
+    }
+return count;
+}
+
+int number = ReadInt();
+int res = number_count(number);
+Console.WriteLine(res);
