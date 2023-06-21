aed55eb61276aece7cc0c23bb42f71b794ac2aa6
PtycaOo <DenKuzia5155@gmail.com>
2023-06-21 23:54:07 +0300

    Кривое решение задачи

---
+++ Seminar4/HomeWork_4/Task_25/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int ReadInt(){
+    Console.WriteLine("Введите число");
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
+}
+
+int new_dig (int A, int B){
+    int product = A;
+    for(int i = 1; i < B; i++){
+        product *= A;  
+    }
+    return product;
+}
+
+int res = new_dig(ReadInt(), ReadInt());
+Console.WriteLine(res);
