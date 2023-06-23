d6079a3a042992c85215bd4ebadfec86de221e4c
PtycaOo <DenKuzia5155@gmail.com>
2023-06-23 21:07:30 +0300

    33 задача 5й семинар

---
+++ Seminar5/Task_33/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int[] GetRandomArr(){
+    int[] arr = new int[4];
+    for(int i = 0; i < arr.Length; i++){
+        arr[i] = Random.Shared.Next(-4,5);
+    }
+    return arr;
+}
+
+int[] ArrReverce(int [] arr){
+    for(int i = 0; i < arr.Length; i++){
+        if(arr[i] < 0){
+            arr[i] = -arr[i];
+        } else if(arr[i] > 0){
+            arr[i] = -arr[i];
+        } else {
+            arr[i] = 0;
+        }
+    }
+    return arr;
+}
+int[] NewArr = GetRandomArr();
+int[] MySumPositive = ArrReverce(NewArr);
+Console.WriteLine(string.Join(", ",MySumPositive));
