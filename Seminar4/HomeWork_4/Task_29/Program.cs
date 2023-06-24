78fae8344833de985501403e4539798d667eccab
PtycaOo <DenKuzia5155@gmail.com>
2023-06-24 16:55:10 +0300

    рандомный массив

---
+++ Seminar4/HomeWork_4/Task_29/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] GetRandomArr(){
+    int[] arr = new int[8];
+    for(int i = 0; i < arr.Length; i++){
+        arr[i] = Random.Shared.Next(0,9);
+    }
+    return arr;
+}
+
+int [] arr = GetRandomArr();
+Console.WriteLine(string.Join(", ", arr));
