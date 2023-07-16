2165d3afb2234cf194f46da68351c03995d50895
PtycaOo <DenKuzia5155@gmail.com>
2023-07-16 22:15:42 +0300

    Сумма всех чисел последовательности

--- Seminar9/Task_66/Program.cs
+++ Seminar9/Task_66/Program.cs
@@ -1,15 +1,16 @@
-﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
-N = 5 -> "5, 4, 3, 2, 1"
-N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+﻿
+/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
 
-Console.WriteLine("Введите начало отсчета: ");
-int x = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите конечное число: ");
-int y = Convert.ToInt32(Console.ReadLine());
-int rec(int x, int y){
-    if(x > y) return 0;
-    return x + rec(x+1,y);
-    
-    
+
+int Perechislenie(int FirstNumber, int LastNumber)
+{
+    if(FirstNumber >= LastNumber) return FirstNumber;
+    return FirstNumber + Perechislenie(FirstNumber + 1, LastNumber);
 }
-Console.WriteLine($"Сумма чисел всей последовательности: {rec(x,y)}");
+
+int M = Convert.ToInt32(Console.ReadLine());
+int N = Convert.ToInt32(Console.ReadLine());
+int S = Perechislenie(M, N);
+Console.Write("Сумма цифр = " + S);
